ac28be3adc360a71392d99835a8a3ac987a92e39
Giuseppe Napoli <giuseppe.napoli1993@hotmail.it>
2021-04-06 15:34:16 +0200

    Terminato

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var suoNome = prompt('Scrivi il tuo Nome?');
+var suoCognome = prompt('Scrivi il tuo Cognome?');
+var suoColore = prompt('Scrivi il tuo colore preferito?');
+var suaPassword = "La tua password: " + suoNome + suoCognome + suoColore + "21";
+
+document.getElementById('pass').innerHTML = suaPassword
